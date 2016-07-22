f99c6dabaade2c10e997185ba70706619221567d
Alberto19 <junioralberto19@gmail.com>
2016-07-22 15:48:11 -0300

    Iniciando com Angular JS

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function(){
+
+	var foto = {
+		titulo: 'Leão',
+		Url: 'http://www.osmais.com/wallpapers/201304/leao-magnifico-wallpaper.jpg' 
+	};
+});

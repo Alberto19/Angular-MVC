0a96ccc06fbd1320a53d65f3a184eff4c5eb96b5
Alberto19 <junioralberto19@gmail.com>
2016-07-22 16:50:42 -0300

    Pegando fotos do BD via Json e enviando para a View

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,28 @@
-angular.module('alurapic').controller('FotosController', function(){
+angular.module('alurapic').controller('FotosController', function($scope, $http){
 
-	var foto = {
-		titulo: 'Leão',
-		Url: 'http://www.osmais.com/wallpapers/201304/leao-magnifico-wallpaper.jpg' 
-	};
+	$scope.fotos = [
+		{
+			titulo: 'Leão',
+			url: 'http://www.labcriativo.com.br/wp-content/uploads/2013/04/As-melhores-capas-para-o-Facebook-43.jpg' 
+		},
+		{
+			titulo: 'Leão 2',
+			url: 'http://www.labcriativo.com.br/wp-content/uploads/2013/04/As-melhores-capas-para-o-Facebook-43.jpg' 
+		},
+	];
+
+	$http.get('v1/fotos').success(function(fotos){
+		$scope.fotos = fotos;
+	}).error(function(erro){
+		console.log(erro);
+	});
+
+
+/*	var promise = $http.get('v1/fotos');
+	promise.then(function(retorno) {
+		$scope.fotos = retorno.data;
+	}).catch(function(error){
+		console.log(error)
+	});
+	*/
 });

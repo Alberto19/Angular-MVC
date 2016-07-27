11b82fba81037dc5bde61a41a3cc56e995b34c0f
Junior <jose.junior@sequenza.com.br>
2016-07-27 14:28:45 -0300

    Aplicando filtro e melhorando o html

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,16 +1,8 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 
-	$scope.fotos = [
-		{
-			titulo: 'Leão',
-			url: 'http://www.labcriativo.com.br/wp-content/uploads/2013/04/As-melhores-capas-para-o-Facebook-43.jpg' 
-		},
-		{
-			titulo: 'Leão 2',
-			url: 'http://www.labcriativo.com.br/wp-content/uploads/2013/04/As-melhores-capas-para-o-Facebook-43.jpg' 
-		},
-	];
-
+	$scope.fotos = [];
+	$scope.filtro = '';
+	
 	$http.get('v1/fotos').success(function(fotos){
 		$scope.fotos = fotos;
 	}).error(function(erro){
